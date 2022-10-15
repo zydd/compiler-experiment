b31bfe7480b110e8fc4564e13cfc29fc27daf817
Gabriel Machado <gabriel_machado@live.com>
2022-10-15 10:55:43 +0200

    Print overall result

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -7,8 +7,16 @@ target="$(dirname $0)/../target/debug/lisp.exe"
 
 $cargo build
 
+overall=true
+
 for test in $path/test_*.ss; do
     # echo -e "* \e[;34m$test\e[0m"
     echo -n "$target "
-    >/dev/null 2>&1 $target $test && echo -e "$test \e[;32mOK\e[0m" || echo -e "$test \e[;31mFAIL\e[0m"
+    >/dev/null 2>&1 $target $test && echo -e "$test \e[;32mOK\e[0m" || {
+        echo -e "$test \e[;31mFAIL\e[0m"
+        overall=false
+    }
 done
+
+echo
+$overall && echo -e "\e[;32mOK\e[0m" || echo -e "\e[;31mFAIL\e[0m"
